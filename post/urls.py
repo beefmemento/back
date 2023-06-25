c4af7e982cb496aa59cea7db714cb1b9effa9b17
JTH <nex_david@naver.com>
2023-06-25 19:17:05 +0900

    백앤드 찐 완성

--- post/urls.py
+++ post/urls.py
@@ -4,4 +4,12 @@ from .views import PostViewSet
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
 
-urlpatterns = router.urls
+from django.urls import path
+from .views import PostBySubjectView
+
+urlpatterns = [
+    path('posts/<str:subject_title>/', PostBySubjectView.as_view(), name='post-by-subject'),
+    # 다른 url 패턴들...
+]
+urlpatterns += router.urls
+
